9c84cb8d8002e0644daae27029d4580906e5d90a
zzou36 <zzou36@gatech.edu>
2016-07-31 23:21:38 -0400

    end at class 21

--- First_App_on_Youtube/music/views.py
+++ First_App_on_Youtube/music/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
-from django.http import HttpResponse
+# from django.http import Http404
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -17,10 +16,11 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    # try:
+    #     album = Album.objects.get(pk=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
 
