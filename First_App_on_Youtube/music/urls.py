c4cb210850d8e48fef56ec483b718547263f541b
zzou36 <zzou36@gatech.edu>
2016-08-02 22:31:06 -0400

    end at class 22

--- First_App_on_Youtube/music/urls.py
+++ First_App_on_Youtube/music/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     # /music/
     url(r'^$', views.index, name='index'),
 
-    # /music/71/
+    # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /music/<album_id>/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
